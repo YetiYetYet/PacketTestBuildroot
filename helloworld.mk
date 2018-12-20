c5d65ce0f347d7ae15a5252a631d31c71418378a
YetiYetYet <alexisbehier22@gmail.com>
2018-12-20 06:32:48 +0100

    first commit

---
+++ helloworld.mk
@@ -0,0 +1,26 @@
+#
+# helloworld
+# basic building rules
+#
+HELLOWORLD_VERSION = 1.1
+HELLOWORLD_SOURCE = helloworld-$(HELLOWORLD_VERSION).tar.xz
+HELLOWORLD_SITE = http://helloworld.com/dl/$(HELLOWORLD_SOURCE)
+
+HELLOWORLD_INSTALL_STAGING = NO
+HELLOWORLD_INSTALL_TARGET = YES
+HELLOWORLD_CONF_OPTS = -DBUILD_DEMOS=ON
+
+define HELLOWORLD_BUILD_CMDS
+    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
+endef
+
+define HELLOWORLD_INSTALL_STAGING_CMDS
+    $(MAKE) DESTDIR=$(STAGING_DIR) -C $(@D) install
+endef
+
+define HELLOWORLD_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/helloworld \
+                          $(TARGET_DIR)/bin/helloworld
+endef
+
+$(eval $(cmake-package))
